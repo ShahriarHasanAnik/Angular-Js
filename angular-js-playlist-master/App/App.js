566f0cee280dfec80ef7a1fa921334cfd3193f20
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2021-04-03 12:27:34 +0600

    Formatted

--- angular-js-playlist-master/App/App.js
+++ angular-js-playlist-master/App/App.js
@@ -50,19 +50,12 @@ $scope.count=0;
                 StudentObj["Department"]=$scope.department;
                 StudentObj["Password"]=$scope.password;
                 StudentObj["Hobby"]=$scope.hobby;
-
-
                 console.log(StudentObj);
                 StudentList.push (StudentObj);
-
                 console.log(StudentList);
-
                 $localStorage.LocalMessage=StudentList;
-
-                  console.log("message")
-
-                  console.log($localStorage.LocalMessage);
-
+                console.log("message")
+                console.log($localStorage.LocalMessage);
                 $scope.gridOptions.data =$localStorage.LocalMessage;
 
 
