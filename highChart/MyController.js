e737d9044d928171e1f18fa77bf308703cc535fa
ShahriarHasanAnik <shahriarhasan2@iut-dhaka.edu>
2021-04-07 09:47:19 +0600

    Array for different chart added

--- highChart/MyController.js
+++ highChart/MyController.js
@@ -1,11 +1,37 @@
 app.controller('MyController', function($scope,$rootScope, $localStorage, $sessionStorage, $window, $http, uiGridConstants) {
 
-var highArray=[1,22,3,4,5,6,6];
-
 $scope.choose=function(para)
 {
+var highArray=[];
+var param="";
 $scope.param = para
+if($scope.param=="bar")
+{
+highArray.push(1,2,3,4,5,6);
+console.log(highArray);
+}
+else if($scope.param=="pie")
+{
+highArray.push(5,7,3,34,5,6);
+console.log(highArray);
+}
+else if($scope.param=="scatter")
+{
+highArray.push(5,7,5,34,4,6);
+console.log(highArray);
+}
+else if($scope.param=="areaspline")
+{
+highArray.push(5,7,5,34.4,100,6);
+console.log(highArray);
+}
+else if($scope.param=="line")
+{
+highArray.push(102,1,5,34.4,100,6);
+console.log(highArray);
+}
 console.log($scope.param);
+
 Highcharts.chart('container', {
   chart: {
     plotBackgroundColor: null,
@@ -17,7 +43,7 @@ Highcharts.chart('container', {
     text: 'My Score As a Batsman'
   },
   tooltip: {
-    pointFormat: '{series.name}: <b>{point.percentage:.1f}</b>'
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
   },
   accessibility: {
     point: {
